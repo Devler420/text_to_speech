685a71042861208463418d0cb4235c0a8fb9480c
Boom P <boompanupong@gmail.com>
2024-08-27 13:51:02 +0700

    add logic to not read duplines

--- main.py
+++ main.py
@@ -4,14 +4,24 @@ import pyttsx3
 
 engine = pyttsx3.init()
 
-print("Starting Text_To_Speech")
+print("Starting Text_To_Speech dot dot dot")
+engine.say("Starting Text To Speech program")
 
 with open("text_to_read.txt", "r") as file:
     lines = file.readlines()
+    print("Total Sentences: " + str(len(lines)))
 
-while True:
+done_read_sentences = set()
+
+while len(done_read_sentences) < len(lines):
     random_line = random.choice(lines)
-    print(random_line)
-    engine.say(random_line)
-    engine.runAndWait()
-    time.sleep(5)
+    if random_line not in done_read_sentences:
+        print(random_line)
+        done_read_sentences.add(random_line)
+        engine.say(random_line)
+        engine.runAndWait()
+        time.sleep(5)
+
+engine.say("Ending Text To Speech program dot dot dot")
+engine.runAndWait()
+print("Ending Text_To_Speech")
