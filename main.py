6b9989a5df05e9fe972f328191a4e5d06d27c95c
Boom P <boompanupong@gmail.com>
2024-09-01 16:28:56 +0700

    spacebar key to read question and answer

--- main.py
+++ main.py
@@ -1,11 +1,13 @@
 import random
 import time
 import pyttsx3
+import keyboard
 
 engine = pyttsx3.init()
 
 print("Starting Text_To_Speech dot dot dot")
-engine.say("Starting Text To Speech program")
+engine.say("Starting Text To Speech program. Press spacebar to continue")
+engine.runAndWait()
 
 with open("text_to_read.txt", "r") as file:
     lines = file.readlines()
@@ -13,15 +15,29 @@ with open("text_to_read.txt", "r") as file:
 
 done_read_sentences = set()
 
+question_counter = 0
 while len(done_read_sentences) < len(lines):
     random_line = random.choice(lines)
     if random_line not in done_read_sentences:
-        print(random_line)
+        if question_counter == 10:
+            engine.say("Ten questions completed")
+            engine.runAndWait()
+        sentence_split = random_line.split('.')
+        question = sentence_split[0]
+        answer = sentence_split[1]
+        keyboard.wait('space')
         done_read_sentences.add(random_line)
-        engine.say(random_line)
+        engine.say(question)
         engine.runAndWait()
-        time.sleep(5)
+        keyboard.wait('space')
+        engine.say(answer)
+        engine.runAndWait()
+        print("Question: " + question)
+        print("Answer: " + answer)
+        question_counter += 1
+        print("Q counter: " + str(question_counter))
+        # time.sleep(5)
 
-engine.say("Ending Text To Speech program dot dot dot")
+engine.say("Ran out of Questions. Ending Text To Speech program dot dot dot")
 engine.runAndWait()
 print("Ending Text_To_Speech")
