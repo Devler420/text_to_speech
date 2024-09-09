de270d214746fac1ce77e463e95669a911134a53
Boom P <boompanupong@gmail.com>
2024-09-09 18:32:39 +0700

    set voice speed

--- main.py
+++ main.py
@@ -3,8 +3,14 @@ import pyttsx3
 import keyboard
 
 engine = pyttsx3.init()
+
+# voice sound
 voices = engine.getProperty('voices')
 engine.setProperty('voice', voices[1].id)
+
+# voice speed -- Default 200 wpm
+newVoiceRate = 160
+engine.setProperty('rate', newVoiceRate)
 
 print("Starting Text_To_Speech dot dot dot")
 engine.say("Starting Text To Speech program. Press spacebar to continue")
@@ -16,8 +22,11 @@ with open("text_to_read.txt", "r") as file:
 
 done_read_sentences = set()
 
-question_counter = 0
+question_counter = 1
 while len(done_read_sentences) < len(lines):
+    # if keyboard.is_pressed("ctrl"):
+    #         print("You pressed CTRL")
+    #         break
     random_line = random.choice(lines)
     if random_line not in done_read_sentences:
         if question_counter == 10:
@@ -25,20 +34,20 @@ while len(done_read_sentences) < len(lines):
             engine.runAndWait()
         sentence_split = random_line.split('|')
         question = sentence_split[0]
-        answer = sentence_split[1]
+        answer = sentence_split[1].strip()
         keyboard.wait('space')
         done_read_sentences.add(random_line)
         engine.say(question)
+        print("Question " + str(question_counter) + ": "  + question)
         engine.runAndWait()
         keyboard.wait('space')
         engine.say(answer)
-        engine.runAndWait()
-        print("Question: " + question)
         print("Answer: " + answer)
+        engine.runAndWait()
         question_counter += 1
-        print("Q counter: " + str(question_counter))
+        print("")
         # time.sleep(5)
 
-engine.say("Ran out of Questions. Ending Text To Speech program dot dot dot")
+engine.say("Ending Text To Speech program")
 engine.runAndWait()
 print("Ending Text_To_Speech")
